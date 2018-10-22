426ddba21399418c23fcc1818cf7843c919f398c
Tran Kim Thanh <kimthanh932000@gmail.com>
2018-10-22 16:42:09 +0700

    create StAXPArser class

---
+++ src/sample/parser/StAXParser.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample.parser;
+
+import javax.xml.stream.XMLEventReader;
+import sample.jaxb.category.Category;
+import sample.jaxb.product.ListProduct;
+
+/**
+ *
+ * @author Administrator
+ */
+public class StAXParser {
+    
+    //get reader
+    public static XMLEventReader getReader(String content){
+        return null;
+    }
+    
+    //get category
+    public static Category parseCategory(String content){
+        return null;
+    }
+    
+    //get list of product
+    public static ListProduct parseListProduct(String content){
+        return null;
+    }
+    
+}
